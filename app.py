b6d5e51b90160ae537a7e321028ce2dbcc66f09c
João Marcelo <joao.marcelo2221@gmail.com>
2023-12-29 21:43:50 -0300

    fim projeto

---
+++ app.py
@@ -0,0 +1,36 @@
+print('----- CALCULADORA INTELIGENTE -----')
+print('=-=' * 13)
+valor1 = int(input('Primeiro valor: '))
+valor2 = int(input('Segundo valor: '))
+operacao = 0
+while operacao != 6:
+
+    print('''     [ 1 ] Somar  
+     [ 2 ] Subtrair  
+     [ 3 ] Multiplicar  
+     [ 4 ] Dividir  
+     [ 5 ] Escolha outros valores  
+     [ 6 ] Sair do programa  ''')
+
+    operacao = int(input('Escolha a operação desejada: '))
+    if operacao == 1:
+        soma = valor1 + valor2
+        print(f'A soma entre {valor1} e {valor2} é = {soma}')
+    elif operacao == 2:
+        subtracao = valor1 - valor2
+        print(f'A diferença entra {valor1} e {valor2} é = {subtracao}')
+    elif operacao == 3:
+        multiplicacao = valor1 * valor2
+        print(f'O resultado de {valor1} X {valor2} é = {multiplicacao}')
+    elif operacao == 4:
+        divisao = valor1 / valor2
+        print(f'O resultado de {valor1} % {valor2} é = {divisao}')
+    elif operacao == 5:
+        valor1 = int(input('Digite um valor: '))
+        valor2 = int(input('Digite outro valor: '))
+    elif operacao == 6:
+        print('Finalizando....')
+    else:
+        print('Opção inválida, tente novamente')
+    print('=-=' * 13)
+print('Fim do programa, volte sempre!')
